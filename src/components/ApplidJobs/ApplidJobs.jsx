e61ad2f88acdd643b634f6d22b467e8c2b1dab38
phlabib <ph.labib@gmail.com>
2023-09-22 20:52:31 +0600

    Set Nested Route and Navbar for CareerHub

---
+++ src/components/ApplidJobs/ApplidJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const ApplidJobs = () => {
+    return (
+        <div>
+            <h2>Jobs I applid</h2>
+        </div>
+    );
+};
+
+export default ApplidJobs;
--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,24 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root';
 import Home from './components/Home/Home';
+import ApplidJobs from './components/ApplidJobs/ApplidJobs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement : <ErrorPage></ErrorPage>,
     children : [
       {
         path : '/',
         element : <Home></Home>
-      }
+      },
+      {
+        path : '/applid',
+        element : <ApplidJobs></ApplidJobs>
+      },
+
     ]
   },
 ]);
