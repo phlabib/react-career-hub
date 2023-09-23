1ca25ce1b211348f4d5adfb28fd45d5837c7cffe
phlabib <ph.labib@gmail.com>
2023-09-23 20:56:44 +0600

    47-5 Add Icons and handle Show All jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,8 @@ const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
 
+    const [dataLength, setDataLength] = useState(4);
+
     useEffect(() => {
         fetch('jobs.json')
         .then (res => res.json())
@@ -17,11 +19,15 @@ const FeaturedJobs = () => {
                 <p>Explore thousands of job opportunities with all the information you need. Its your future
                 </p>
             </div>
-            <div className="grid grid md:grid-cols-2 gap-6 mb-6 mt-6">
+            <div className="grid md:grid-cols-2 gap-6 mb-6 mt-6">
                 {
-                    jobs.map(job => <Job key = {job.id} job = {job}></Job> )
+                    jobs.slice(0, dataLength).map(job => <Job key = {job.id} job = {job}></Job> )
                 }
             </div>
+           <div className={dataLength === jobs.length && 'hidden'}>
+           <button
+            onClick={() => setDataLength(jobs.length)} className="btn btn-primary ">Show All Jobs</button>
+           </div>
         </div>
     );
 };
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -4,7 +4,7 @@ import { GrLocation , GrCurrency} from "react-icons/gr";
 const Job = ({job}) => {
     const {logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
-        <div className="card card-compact bg-base-100 shadow-xl">
+        <div className="card card-compact bg-slate-200 shadow-xl py-4 ">
         <figure><img src= {logo} alt="Shoes" /></figure>
         <div className="card-body">
           <h2 className="card-title">{job_title}</h2>
