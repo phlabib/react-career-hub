ea3b9fe0632228fb70e206e9730dcd2117030fcd
phlabib <ph.labib@gmail.com>
2023-09-23 12:02:10 +0600

    47-3 Load Jobs data for the page

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,12 +1,26 @@
-
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('jobs.json')
+        .then (res => res.json())
+        .then(data => setJobs(data));
+    }, [])
     return (
         <div>
             <div className="text-center ">
-                <h2 className="text-6xl mt-6 mb-6">Featured Jobs</h2>
+                <h2 className="text-6xl mt-6 mb-6">Featured Jobs : {jobs.length}</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future
                 </p>
+            </div>
+            <div>
+                {
+                    jobs.map(job => <Job key = {job.id} job = {job}></Job> )
+                }
             </div>
         </div>
     );
